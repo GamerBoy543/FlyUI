be3a7e28a89e027dfa70616e5acd540872d07d1a
GamerBoy543 <83164805+GamerBoy543@users.noreply.github.com>
2021-04-26 12:03:05 +0200

    Update Main.php

--- src/FlyUI/Main.php
+++ src/FlyUI/Main.php
@@ -4,9 +4,7 @@ namespace FlyUI;
 
 use pocketmine\Server;
 use pocketmine\Player;
-
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
